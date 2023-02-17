72119a3de8a2fb41fa39b475cfeb693e2f2280f2
bitorhugo <vitorhugosantos144@gmail.com>
2023-02-17 16:23:24 +0000

    added arguments

--- build.sh
+++ build.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/bash
 
-PROJECTNAME=
-PROJECTROOT=
+# usage: jcompile -n <project name> -r <project root>
+while [[ $# -gt 0 ]]; do
+    command=$1
+    input=$2
+    shift
+    shift
+    
+    case ${command} in
+        -n) PROJECTNAME=$input
+            echo "name :${PROJECTNAME}";;
+        -r) PROJECTROOT=$input
+            echo "root: ${PROJECTROOT}";;
+         *)  echo "Invalid arguments"
+    esac
+done
 
 find ${PROJECTROOT} -name "*.java" >> ${PROJECTROOT}/modules
 javac @${PROJECTROOT}/modules -d ${PROJECTROOT}/out/
